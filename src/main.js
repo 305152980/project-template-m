6f3b3cf069e912dc586bf9cfd4792ae8de24300f
lijie <lj19102837815@163.com>
2022-11-13 17:20:35 +0800

    日常提交

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 加载 Vant 组件库。
 import Vant from 'vant'
+// 加载 Vant 全局样式。
 import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
 
+// 注册 Vant 组件库。
 Vue.use(Vant)
 
 new Vue({
