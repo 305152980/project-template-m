3389b4eb7b8e7087d333eaa8a1535917c3617478
lijie <lj19102837815@163.com>
2022-11-27 22:16:26 +0800

    日常提交

---
+++ src/apis/login/index.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+export function login(params) {
+  return request('/app/v1_0/authorizations', 'POST', params)
+}
+
+export function sendSms(mobile) {
+  return request(`/app/v1_0/sms/codes/${mobile}`, 'GET')
+}
+
+export function queryUserInfo() {
+  return request('/app/v1_0/user', 'GET')
+}
--- vue.config.js
+++ vue.config.js
@@ -18,14 +18,11 @@ module.exports = {
     // 配置请求的代理服务器，解决开发阶段的跨域问题。
     proxy: {
       '/dev-api': {
-        // 目标服务器的地址。
-        // 测试 Axios 请求的目标服务器地址。
         target: 'http://ttapi.research.itcast.cn',
         // 支持 websocket 请求的代理。
         ws: true,
         // 代理服务器转发请求时请求头中的 host 值是否伪装。
         changeOrigin: true,
-        // localhost:8080/dev-api/login ==> http://......:8080/login
         pathRewrite: {
           '^/dev-api': ''
         }
