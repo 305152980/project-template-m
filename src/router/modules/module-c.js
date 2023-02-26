e92be86cba8c581155612fbe74c89b0bd83b54ad
lijie <lj19102837815@163.com>
2023-02-26 22:44:24 +0800

    日常提交

--- src/router/modules/module-c.js
+++
@@ -1,9 +0,0 @@
-const moduleC = () => import('@/views/module-c/index.vue')
-
-export default [
-  {
-    path: '/moduleC',
-    name: 'ModuleC',
-    component: moduleC
-  }
-]
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -24,7 +24,7 @@
 
 <script>
 export default {
-  name: 'LayoutIndex'
+  name: 'Layout'
 }
 </script>
 
