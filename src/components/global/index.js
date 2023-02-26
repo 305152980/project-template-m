dddf0043828d0c2f4099528af2978a0d987ee4a5
lijie <lj19102837815@163.com>
2023-02-26 16:46:15 +0800

    日常提交

--- src/components/global/index.js
+++ src/components/global/index.js
@@ -1,4 +1,5 @@
-// 使用 require 提供的 context 函数，加载指定目录下的所有 .vue 文件。context 函数返回一个 importFn 函数。
+// 使用 require 提供的 context 函数，加载指定目录下的所有 .vue 文件。
+// context 函数返回一个 importFn 函数。
 // importFn 函数的 keys() 属性的值为这些被指定加载的 .vue 文件的文件路径数组。
 // 遍历文件路径数组，再次使用 importFn 函数根据文件路径导入组件对象。
 // 获得组件对象后对其进行全局注册。
@@ -8,8 +9,8 @@ const importFn = require.context('./', false, /\.vue$/)
 
 export default {
   install(Vue) {
-    importFn.keys().forEach(path => {
-      const component = importFn(path).default
+    importFn.keys().forEach(filePath => {
+      const component = importFn(filePath).default
       Vue.component(component.name, component)
     })
   }
